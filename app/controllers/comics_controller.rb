67425a2856e443ff3220226bf5aa199da11e5621
arnaud longueval <arnaud.longueval@edhec.com>
2016-11-14 20:13:23 +0100

    creation des methods comics

---
+++ app/controllers/comics_controller.rb
@@ -0,0 +1,22 @@
+class ComicsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def detroy
+  end
+end
