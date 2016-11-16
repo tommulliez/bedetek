c999b53bc1d9ec1fd62b5cfdbaedd8ca775a4663
arnaud longueval <arnaud.longueval@edhec.com>
2016-11-16 09:11:31 +0100

    done

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -8,7 +8,7 @@ class Comic < ApplicationRecord
   validates :price, presence: true
   # validates :photo, presence: true
   validates :genre, inclusion: { in: ["adventure", "aviation", "fantastic", "history", "science-fiction", "western", "thriller", "adult", "manga"], allow_nil: false }
-  validates :state, inclusion: { in: ["Good condition", "Average condition", "Bad condition"], allow_nil: false }
+  validates :state, inclusion: { in: ["Like it was new", "Average used", "Almost a wreck"], allow_nil: false }
 
   mount_uploader :photo, PhotoUploader
 end
